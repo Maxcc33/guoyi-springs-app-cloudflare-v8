c74bc2ca277f265e556eccf0445fe5925ba5cd22
Maxcc33 <1926375372cgy@gmail.com>
2026-03-12 14:59:54 +0800

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,6 +20,7 @@ const App = () => (
       <LanguageProvider>
         <Toaster />
         <BrowserRouter>
+          <ScrollToTop />
           <div className="flex flex-col min-h-screen">
             <Navbar />
             <main className="flex-grow">
